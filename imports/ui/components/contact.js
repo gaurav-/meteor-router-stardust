f7cc9d18814ed6073b4c775b7300059c3abfcb71
cristiandley <cristiandley@gmail.com>
2016-09-07 10:51:04 -0300

    pkgjson update

--- imports/ui/components/contact.js
+++ imports/ui/components/contact.js
@@ -8,26 +8,28 @@ class Contact extends Component {
   }
 
   render() {
+
     return (
         <div>
-        <h1>Just another example Route</h1>
-        <Form>
-          <Form.Field>
-            <label>First Name</label>
-            <input placeholder='First Name' />
-          </Form.Field>
-          <Form.Field>
-            <label>Last Name</label>
-            <input placeholder='Last Name' />
-          </Form.Field>
-          <Form.Field>
-            <label>First Name</label>
-            <Checkbox label='I agree to agree' />
-          </Form.Field>
-          <Button type='submit'>Submit</Button>
-        </Form>
+          <h1>Just another example Route</h1>
+          <Form>
+            <Form.Field>
+              <label>First Name</label>
+              <input placeholder='First Name' />
+            </Form.Field>
+            <Form.Field>
+              <label>Last Name</label>
+              <input placeholder='Last Name' />
+            </Form.Field>
+            <Form.Field>
+              <label>First Name</label>
+              <Checkbox label='I agree to agree' />
+            </Form.Field>
+            <Button type='submit'>Submit</Button>
+          </Form>
         </div>
     );
+
   }
 
 }
